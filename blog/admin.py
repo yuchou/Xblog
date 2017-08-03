b269ae03c6cedfc577616770b7ce57854ea8517d
zouyu <zouyu/user@youlu>
2017-08-03 16:47:52 +0800

    [U] 去掉不必要的注释

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Category, Tag, Post
-# Register your models here.
 
 
 class PostAdmin(admin.ModelAdmin):
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     return render(request, 'blog/index.html', context={'title':'测试', 'welcome': 'Hello,world'})
