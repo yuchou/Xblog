62c89b30ab92a4b7fe0ac1978999b3ff06889fc7
zouyu <zouyu/user@youlu>
2017-08-04 09:57:53 +0800

    [U] 主页和详情页

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 def index(request):
-    return render(request, 'blog/index.html', context={'title':'测试', 'welcome': 'Hello,world'})
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', context={'post': post})
--- xblog/settings.py
+++ xblog/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics'),
+     os.path.join(BASE_DIR, "statics"),
 ]
 
