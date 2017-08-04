b608e847c9dc06affddf57e761fc53d69e10df9c
zouyu <zouyu/user@youlu>
2017-08-04 10:04:55 +0800

    [U] Post数据表显示处理

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,6 @@ from .models import Category, Tag, Post
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
 
-admin.sites.site.register(Post)
+admin.sites.site.register(Post, PostAdmin)
 admin.sites.site.register(Category)
 admin.sites.site.register(Tag)
