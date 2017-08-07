b8ed21f6e802e268400915b71f1b7359d27a6a19
zouyu <zouyu/user@youlu>
2017-08-07 11:33:59 +0800

    [U] 归档页面显示处理

--- blog/views.py
+++ blog/views.py
@@ -17,3 +17,10 @@ def detail(request, pk):
                                       'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
