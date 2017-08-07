0da65d788978e3c0e5d2e10ce0245c36723a1f07
zouyu <zouyu/user@youlu>
2017-08-07 16:35:16 +0800

    [U] comments数据库模型

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    name = models.CharField(max_length=100, verbose_name='读者')
+    email = models.EmailField(max_length=255, verbose_name='邮箱')
+    url = models.URLField(blank=True, verbose_name='网址')
+    text = models.TextField(verbose_name='评论')
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
+
+    post = models.ForeignKey('blog.Post')
+
+    def __str__(self):
+        return self.text[:20]
