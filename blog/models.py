bbcb828c3ad3e977c77d62c078f488b6c8ab289c
zouyu <zouyu/user@youlu>
2017-08-08 10:12:03 +0800

    [U] 文章摘要显示

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
 
 
 class Category(models.Model):
@@ -45,6 +47,15 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = '文章'
